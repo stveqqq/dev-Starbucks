026926d0ccfa3cf2f0c6c59715eef4363a7286a8
Your Name <you@example.com>
2025-03-15 16:08:57 +0200

    header done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,7 @@
-  
+const burger = document.querySelector('.btn-burger');
+const mobileContainer = document.querySelector('.mobile-container')
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    mobileContainer.classList.toggle('show')
+})
