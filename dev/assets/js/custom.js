dce21e6f6a09aaf6dbc0b723b6dac5ebe0608730
Your Name <you@example.com>
2025-05-15 20:18:31 +0300

    feat: added setting to product swiper

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -10,3 +10,16 @@ burger.addEventListener('click', toggleMobileContainer)
 
 const label = document.querySelectorAll('.label')
 const btn = document.querySelectorAll('.btn')
+
+
+const swiper = new Swiper('.swiper-product', {
+    slidesPerView: 4,
+    loop: true,
+    speed: 500,
+    spaceBetween: 48,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
